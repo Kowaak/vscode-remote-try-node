89a5e16a053005e08a2f20292ddd938a5756b584
Kowaak <102755037+Kowaak@users.noreply.github.com>
2024-03-22 10:54:58 +0100

    Update app2.js

--- app2.js
+++ app2.js
@@ -47,7 +47,58 @@ var myEventHandler = function () {
 eventEmitter.on('scream', myEventHandler);
 
 eventEmitter.emit('scream');
-//6
+//6 wszystko z lekcji
+var http = require('http');
+var fs = require('fs');
+http.createServer(function(req,res){
+    res.writeHead(200,{'content-type':'text/html'});
+    res.writeHead(200,{'content-type':'application/json'})
+    if (req.url === '/'){
+        //var html = fs.readFileSync('./plik.html','utf8');
+        //var Header = 'Home page';
+        //html = html.replace('{ Header }',Header);
+        //res.end(html);
+
+        res.writeHead(200);
+
+    }else if(req.url === '/product'){/
+        res.writeHead(200);
+        res.end('Products');
+    }else if(req.url === '/blog'){
+        res.writeHead(200);
+        res.end('Blog');
+    }else{
+        res.writeHead(404);
+        res.end('404: Not found');
+    }
+    
+    
+    fs.createReadStream('./plik.html').pipe(res);
+    res.writeHead(200);
+    res.end(html);
+    
+}).listen(3000);
+console.log("Server on")
+
+//express
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Home');
+})
+app.get('/contact',function(req,res){
+    res.send('contact')
+})
+app.get('/product/:id',function(req,res){
+    res.send('id: ' + req.params.id)
+})
+app.get('/other',function(req,res){
+    res.send('other')
+})
+app.listen(3000);
+
+
 
 
 
